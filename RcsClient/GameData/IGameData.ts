d4e4d93f76d91bda0c200475a7777b8982095132
Jorge <jogonzal@users.noreply.github.com>
2018-07-04 13:19:02 -0700

    Game data compile time validation

---
+++ RcsClient/GameData/IGameData.ts
@@ -0,0 +1,33 @@
+type PlayerName = 'AndreasH' |
+'ChrisB'|
+'DiegoV'|
+'DineshV'|
+'JordanH'|
+'JasonR'|
+'JorgeA'|
+'JorgeM'|
+'MarkT'|
+'MartinM'|
+'MattA'|
+'MattMC'|
+'MattR'|
+'NicoS'|
+'RaymondO'|
+'ScottO'|
+'UmairA'|
+'Khalef'
+
+export interface IPlayerGameData {
+    Goals: number
+    Assists: number
+    Comment?: string
+}
+
+export interface IGameData {
+    Opponent: string
+    Date: string
+    GoalsInFavor: string
+    GoalsAgainst: string
+    Notes: string
+    PlayerStats: { [key in PlayerName]?: IPlayerGameData }
+}
