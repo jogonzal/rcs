f1d322b5e0cb954f42a525a7c3921c95d1bb58c4
Jorge <jogonzal@users.noreply.github.com>
2018-07-04 12:31:59 -0700

    Remove stuff that's not used

--- RcsServer/RcsServer/Startup.cs
+++ RcsServer/RcsServer/Startup.cs
@@ -31,11 +31,11 @@ namespace RcsServer
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseWebpackDevMiddleware(new WebpackDevMiddlewareOptions
-                {
-                    HotModuleReplacement = true,
-                    ReactHotModuleReplacement = true
-                });
+                //app.UseWebpackDevMiddleware(new WebpackDevMiddlewareOptions
+                //{
+                //    HotModuleReplacement = true,
+                //    ReactHotModuleReplacement = true
+                //});
             }
             else
             {
