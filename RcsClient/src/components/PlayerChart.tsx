e2ebf7880af6439e4b4fb52b390b1210b2b65c05
Jorge <jogonzal@users.noreply.github.com>
2018-07-04 17:49:45 -0700

    Add one more TODO

--- RcsClient/src/components/PlayerChart.tsx
+++ RcsClient/src/components/PlayerChart.tsx
@@ -19,6 +19,7 @@ export default class PlayerChart extends React.Component<Props, State> {
     render() {
         const dataForChart = getAggregatedPlayerDataForAllGames(this.props.data, this.props.field, this.props.N)
 
+        // TODO: tweak to show pictures in bar by tweaking the label component, like this: http://recharts.org/#/en-US/api/Label
         return (
             <>
                 <h2>Top {this.props.N} {this.props.field}</h2>
