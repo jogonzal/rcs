1995203c337d7b06085c8fd91546ad4f030b567f
Jorge <jogonzal@users.noreply.github.com>
2018-07-04 23:01:20 -0700

    raw data should be everything

--- RcsClient/src/components/RawData.tsx
+++ RcsClient/src/components/RawData.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import './../assets/scss/App.scss'
 import NavBar from './NavBar'
-import spring2018Data from '../../GameData/Spring2018Data'
+import availableSeasons from '../stats/availableSeasons'
 
 const RawData = () => (
   <div>
@@ -9,7 +9,7 @@ const RawData = () => (
     <div className='container'>
         <p>Not sure why you'd want this, but here it is:</p>
         <pre>
-            {JSON.stringify(spring2018Data, undefined, 2)}
+            {JSON.stringify(availableSeasons, undefined, 2)}
         </pre>
     </div>
   </div>
