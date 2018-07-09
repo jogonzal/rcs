c07732cb5d0d33be893a014ebbe7566e0f357a59
Jorge <jogonzal@users.noreply.github.com>
2018-07-08 22:14:55 -0700

    Fix build

--- RcsClient/src/components/CustomAxisTick.tsx
+++ RcsClient/src/components/CustomAxisTick.tsx
@@ -9,15 +9,17 @@ const CustomAxisTick = (props) => {
     const pictureUrl = PlayerPics.GetPic(playerName)
 
     const sizeOfImage = 40
-    return <image xlink:href={pictureUrl}
-        height={sizeOfImage} width={sizeOfImage}
-        x={x - sizeOfImage / 2} y={y - 12} />
-    
+    const myHtml = `<image xlink:href="${pictureUrl}"
+        height="${sizeOfImage}" width="${sizeOfImage}"
+        x="${x - sizeOfImage / 2}" y="${y - 12}" />`
+
     // return (
     //     <svg x={x - 12} y={y + 4} width={24} height={24} viewBox='0 0 1024 1024' fill='#666'>
     //         <path d={path} />
     //     </svg>
     // )
+
+    return ( <g dangerouslySetInnerHTML={{__html: myHtml}}/> )
 }
 
 export default CustomAxisTick
