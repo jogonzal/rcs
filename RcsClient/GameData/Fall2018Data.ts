29f8459d6e9e3a853ec222e092c0665f6807a29d
Jorge <jogonzal@users.noreply.github.com>
2018-08-21 11:33:37 -0700

    Added fall2018

---
+++ RcsClient/GameData/Fall2018Data.ts
@@ -0,0 +1,37 @@
+// tslint:disable:indent
+import { IGameData } from './IGameData'
+
+// {
+// 	Opponent: 'COPYPASTETHISONE',
+// 	Date: '4/11/2018',
+// 	GoalsInFavor: 0,
+// 	GoalsAgainst: 0,
+// 	Notes: 'THIS IS OPTIONAL',
+// 	PlayerStats:
+// 	{
+// 		AndreasH: { 'Goals': 0, 'Assists': 0 },
+// 		ChrisB: { 'Goals': 0, 'Assists': 0 },
+// 		DiegoV: { 'Goals': 0, 'Assists': 0 },
+// 		DineshV: { 'Goals': 0, 'Assists': 0 },
+// 		JordanH: { 'Goals': 0, 'Assists': 0 },
+// 		JasonR: { 'Goals': 0, 'Assists': 0 },
+// 		JorgeA: { 'Goals': 0, 'Assists': 0 },
+// 		JorgeM: { 'Goals': 0, 'Assists': 0 },
+// 		MarkT: { 'Goals': 0, 'Assists': 0 },
+// 		MartinM: { 'Goals': 0, 'Assists': 0 },
+// 		MattA: { 'Goals': 0, 'Assists': 0 },
+// 		MattMC: { 'Goals': 0, 'Assists': 0 },
+// 		MattR: { 'Goals': 0, 'Assists': 0 },
+// 		NicoS: { 'Goals': 0, 'Assists': 0 },
+// 		RaymondO: { 'Goals': 0, 'Assists': 0 },
+// 		ScottO: { 'Goals': 0, 'Assists': 0 },
+// 		UmairA: { 'Goals': 0, 'Assists': 0 }
+// 		Nimish: { 'Goals': 0, 'Assists': 0 }
+// 	}
+// },
+
+const summer2018Data: IGameData[] =
+[
+]
+
+export default summer2018Data
--- RcsClient/src/stats/availableSeasons.ts
+++ RcsClient/src/stats/availableSeasons.ts
@@ -2,10 +2,12 @@ import { IGameData } from '../../GameData/IGameData'
 
 // import spring2018Data from '../../GameData/Spring2018Data'
 import summer2018Data from '../../GameData/Summer2018Data'
+import fall2018Data from '../../GameData/Fall2018Data'
 
 const availableSeasons: { [key: string]: IGameData[] } = {
   // 'Spring 2018': spring2018Data, Data for this one is innacurate
-  'Summer 2018': summer2018Data
+  'Summer 2018': summer2018Data,
+  'Fall 2018' : fall2018Data
 }
 
 export default availableSeasons
