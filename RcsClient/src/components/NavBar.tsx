2d45765bbd78dcd0486088056ad8349e5eb9d715
Jorge <jogonzal@users.noreply.github.com>
2018-11-04 19:33:24 -0800

    Add the navbar to see what's up

--- RcsClient/src/components/NavBar.tsx
+++ RcsClient/src/components/NavBar.tsx
@@ -1,8 +1,43 @@
 import * as React from 'react'
 import { Link } from 'react-router-dom'
+import { Navbar, NavItem, Nav, NavDropdown, MenuItem } from 'react-bootstrap'
 
 const NavBar = () => (
     <>
+    <Navbar inverse collapseOnSelect>
+  <Navbar.Header>
+    <Navbar.Brand>
+      <a href='#brand'>React-Bootstrap</a>
+    </Navbar.Brand>
+    <Navbar.Toggle />
+  </Navbar.Header>
+  <Navbar.Collapse>
+    <Nav>
+      <NavItem eventKey={1} href='#'>
+        Link
+      </NavItem>
+      <NavItem eventKey={2} href='#'>
+        Link
+      </NavItem>
+      <NavDropdown eventKey={3} title='Dropdown' id='basic-nav-dropdown'>
+        <MenuItem eventKey={3.1}>Action</MenuItem>
+        <MenuItem eventKey={3.2}>Another action</MenuItem>
+        <MenuItem eventKey={3.3}>Something else here</MenuItem>
+        <MenuItem divider />
+        <MenuItem eventKey={3.3}>Separated link</MenuItem>
+      </NavDropdown>
+    </Nav>
+    <Nav pullRight>
+      <NavItem eventKey={1} href='#'>
+        Link Right
+      </NavItem>
+      <NavItem eventKey={2} href='#'>
+        Link Right
+      </NavItem>
+    </Nav>
+  </Navbar.Collapse>
+</Navbar>
+
     <nav className='navbar navbar-expand-lg navbar-light bg-light'>
         <Link to='/' className='navbar brand'>
             RCS
